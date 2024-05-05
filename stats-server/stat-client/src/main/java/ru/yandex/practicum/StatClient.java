0ee9a6c3885010cd665556e025f0428db06c3e09
EgorovArtur <arthurmsk12154@yandex.ru>
2024-05-05 19:29:22 +0300

    Реализация дипломного проекта Часть №2

--- stats-server/stat-client/src/main/java/ru/yandex/practicum/StatClient.java
+++ stats-server/stat-client/src/main/java/ru/yandex/practicum/StatClient.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
 import java.time.LocalDateTime;
@@ -26,7 +27,7 @@ public class StatClient extends BaseClient {
         );
     }
 
-    public ResponseEntity<Object> addHit(HitDto hitDto) {
+    public ResponseEntity<Object> addHit(@RequestBody HitDto hitDto) {
         return post("/hit", hitDto);
     }
 
